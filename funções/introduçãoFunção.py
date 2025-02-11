064f4b436f1decaed965934e7a7a7dfe40ceab56
Edu <luis_100404@hotmail.com>
2025-02-10 23:39:57 -0300

    add new files

---
+++ funções/introduçãoFunção.py
@@ -0,0 +1,7 @@
+def seiLa():
+    print("Função sei lá")
+
+def nome(nome):
+    print(f"Olá, {nome}.")
+
+nome("Edu")
---
+++ funções/returnAndFunction.py
@@ -0,0 +1,8 @@
+def soma(a,b):
+    return a + b
+
+soma1 = soma(5,5)
+soma2 = soma(2,2)
+
+soma3 = soma1 + soma2
+print(soma3)
